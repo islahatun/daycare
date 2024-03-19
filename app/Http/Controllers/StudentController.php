016b006444819043ed0fe4e8568f8c0bb9c154a8
islahatun Nufusi <islahatunnufusi07@gmail.com>
2024-03-19 15:42:03 +0700

    setting feature admin

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('cms.students');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+
+    public function getData(){
+        $data   = Student::all();
+
+        return DataTables::of($data)->addIndexColumn()->make(true);
+
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
