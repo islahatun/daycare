20a31fd402c9f6455183eac5fe5bc5024d74b7b9
islahatun Nufusi <islahatunnufusi07@gmail.com>
2024-04-29 09:48:17 +0700

    change language from en to id

--- resources/views/partials/cms/sidebar.blade.php
+++ resources/views/partials/cms/sidebar.blade.php
@@ -9,53 +9,54 @@
         </a>
       </li><!-- End Dashboard Nav -->
 
-      <li class="nav-heading">Pages</li>
+      <li class="nav-heading">Halaman</li>
 
       <li class="nav-item">
         <a class="nav-link collapsed" href="/profile">
           <i class="bi bi-person"></i>
-          <span>Profile</span>
+          <span>Profil</span>
         </a>
       </li><!-- End Profile Page Nav -->
 
       <li class="nav-item">
         <a class="nav-link collapsed" href="/students">
           <i class="bi bi-question-circle"></i>
-          <span>List Students</span>
+          <span>Daftar Anak</span>
         </a>
       </li><!-- End F.A.Q Page Nav -->
 
       <li class="nav-item">
         <a class="nav-link collapsed" href="/teacher">
           <i class="bi bi-envelope"></i>
-          <span>List Teacher</span>
+          <span>Daftar Guru</span>
         </a>
       </li><!-- End Contact Page Nav -->
 
-      <li class="nav-item">
+      {{-- <li class="nav-item">
         <a class="nav-link collapsed" href="/class">
           <i class="bi bi-envelope"></i>
           <span>List Class</span>
         </a>
-      </li><!-- End Contact Page Nav -->
+      </li> --}}
+      <!-- End Contact Page Nav -->
 
       <li class="nav-item">
         <a class="nav-link collapsed" href="/activityChildern">
           <i class="bi bi-card-list"></i>
-          <span>activity Childern</span>
+          <span>Aktivitas Anak</span>
         </a>
       </li><!-- End Register Page Nav -->
       <li class="nav-item">
         <a class="nav-link collapsed" href="/DevelopmentChildern">
           <i class="bi bi-card-list"></i>
-          <span>Development Childern</span>
+          <span>Master Penilaian</span>
         </a>
       </li><!-- End Register Page Nav -->
 
       <li class="nav-item">
         <a class="nav-link collapsed" href="/trans-DevelopmentChildern">
           <i class="bi bi-card-list"></i>
-          <span>Assessment</span>
+          <span>Penilaian Anak</span>
         </a>
       </li><!-- End Register Page Nav -->
 
