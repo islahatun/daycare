cccc8428e033b5af43ae190890c63c942a9c73a8
islahatun Nufusi <islahatunnufusi07@gmail.com>
2024-05-18 12:57:20 +0700

    sidebar

--- resources/views/partials/home/footer.blade.php
+++ resources/views/partials/home/footer.blade.php
@@ -1,14 +1,28 @@
+<!-- JavaScript Libraries -->
 
-    <!-- JavaScript Libraries -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{!! asset('assets-kider/lib/wow/wow.min.js') !!}"></script>
-    <script src="{!! asset('assets-kider/lib/easing/easing.min.js') !!}"></script>
-    <script src="{!! asset('assets-kider/lib/waypoints/waypoints.min.js') !!}"></script>
-    <script src="{!! asset('assets-kider/lib/owlcarousel/owl.carousel.min.js') !!}"></script>
+<!-- Template Main JS File -->
+<script src="{!! asset('assets/js/jquery.min.js') !!}"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="{!! asset('assets-kider/lib/wow/wow.min.js') !!}"></script>
+<script src="{!! asset('assets-kider/lib/easing/easing.min.js') !!}"></script>
+<script src="{!! asset('assets-kider/lib/waypoints/waypoints.min.js') !!}"></script>
+<script src="{!! asset('assets-kider/lib/owlcarousel/owl.carousel.min.js') !!}"></script>
 
-    <!-- Template Javascript -->
-    <script src="{!! asset('assets-kider/js/main.js') !!}"></script>
+<!-- Template Javascript -->
+<script src="{!! asset('assets-kider/js/main.js') !!}"></script>
+
+{{-- datatables --}}
+<script src="{!! asset('assets/datatables/datatables.min.js') !!}"></script>
+
+<!-- Template jquery ui File -->
+<script src="{!! asset('assets/jquery-ui/jquery-ui.min.js') !!}"></script>
+
+<!-- Template toastr File -->
+<script src="{!! asset('assets/toastr/toastr.min.js') !!}"></script>
+
+<!-- Template Main JS File -->
+<script src="{!! asset('assets-home/js/main.js') !!}"></script>
 </body>
 
 </html>
